eba51e8940fe709196b20e3e493b898c876947ab
nayka-mihir <mihirnayka786@gmail.com>
2026-02-07 17:47:53 +0530

    add ui

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './features/home/home';
+import { NavbarComponent } from './shared/components/navbar/navbar';
+import { FooterComponent } from './shared/components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HomeComponent,NavbarComponent,FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
